8e2439b9b931365f9903b62c2315676b1535da62
citra-citoru <citra.rahman@happy5.co>
2023-09-24 19:16:08 +0300

    first commit

--- next.config.js
+++ next.config.js
@@ -1,4 +1,9 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {}
-
-module.exports = nextConfig
+const nextConfig = {
+    images: {
+      domains: ['image.tmdb.org', 'lh3.googleusercontent.com']
+    }
+  }
+  
+  module.exports = nextConfig
+  
